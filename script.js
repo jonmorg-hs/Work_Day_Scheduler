0912e5be0149b9116b7fb1eef08dd7da2c7cba2e
Jon Morgan <jonmorg@iinet.net.au>
2021-09-17 13:15:12 +1000

    updating all files

---
+++ script.js
@@ -0,0 +1,28 @@
+var data = ['','','','','','','','',''];
+if(localStorage.getItem("workday_data")==null){} else {
+data = JSON.parse(localStorage.getItem("workday_data"));
+}
+var currentHour = moment().format('H');
+$('#currentDay').html(moment().format('dddd, MMMM Do'));
+var rowNo = 9;
+var key = 0;
+$('table tr').each(function(){   
+if(rowNo<currentHour){
+$(this).children('td').children('textarea').val(data[key]).attr('class','past');
+} else {
+if(rowNo>currentHour){
+$(this).children('td').children('textarea').val(data[key]).attr('class','future');
+} else {
+$(this).children('td').children('textarea').val(data[key]).attr('class','present');
+}}
+rowNo = rowNo+1;
+key = key+1;
+});
+
+$('.saveBtn').click(function(){
+    data = [];
+    $('table tr').each(function(){ 
+    data.push($(this).children('td').children('textarea').val());    
+    }); 
+    localStorage.setItem("workday_data",JSON.stringify(data));  
+})
